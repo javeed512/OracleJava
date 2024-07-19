4b8334c1ac3fd5721e6bd444f21aa69913e3dcc4
javeed512 <javeedmohammedjaveed@gmail.com>
2024-07-19 13:17:37 +0530

    Using Jar in Java Apps Demo

---
+++ Jar_Packaging_Demo/SchoolApp/src/com/demo/Test.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import com.school.Student;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+
+				Student student = new Student(101, "King");
+				
+				
+				System.out.println(student);
+		
+
+	}
+
+}
